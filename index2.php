df90376a0f84727f93a3738012fd6026c687e722
plasticus <marquarc@gmail.com>
2025-04-16 12:46:52 -0500

    Add kitten image to index2.php

--- index2.php
+++ index2.php
@@ -1,1 +1,4 @@
-<?php echo 'Hello from GitHub push test -- Beep Boop'; ?>
+<?php
+echo "<h1>Hello from GitHub push test!</h1>";
+echo "<img src='cat.jpg' alt='Kitten Test' width='300' height='200'>";
+?>
